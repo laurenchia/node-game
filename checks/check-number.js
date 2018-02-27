72afa4210651d81f4246e1f1281ea0995fd9a920
Lauren Chia <lauren.chia@inlight.com.au>
2018-02-27 17:32:20 +1100

    Remove files

--- checks/check-number.js
+++
@@ -1,4 +0,0 @@
-//Condition 1 - Fails if it IS NOT a number
-module.exports = function(guess) {
-	return !isNaN(guess);
-}
--- checks/check-odd.js
+++
@@ -1,6 +0,0 @@
-//Condition 2 - Fails if it IS NOT odd - remainder is 0 
-module.exports = function(guess) {
-	return guess % 2 !== 0;
-}	
-
-	
